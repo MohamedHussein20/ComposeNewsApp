39f9b91483acdb6b56740c33a40674a35ada1065
Mohamed Hussein <mohussein1279@gmail.com>
2025-03-07 17:13:08 +0200

    sealed class for states has been created

---
+++ utilitis/src/main/java/com/memo/utilitis/ResourceState.kt
@@ -0,0 +1,8 @@
+package com.memo.utilitis
+
+sealed class ResourceState<T> {
+    class Loading<T> : ResourceState<T>()
+    class Success<T>(val data: T) : ResourceState<T>()
+    class Error<T>(val message: String) : ResourceState<T>()
+
+}
