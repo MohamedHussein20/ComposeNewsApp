d69ec983fed4322f51b45b2bcfd7ce03ed98f109
Mohamed Hussein <mohussein1279@gmail.com>
2025-03-07 17:20:54 +0200

    Flow creation for Api response

--- app/src/main/java/com/memo/newsapp/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/memo/newsapp/ui/repository/NewsRepository.kt
@@ -2,13 +2,30 @@ package com.memo.newsapp.ui.repository
 
 import com.memo.newsapp.data.datasource.NewsDataSource
 import com.memo.newsapp.data.entity.NewsResponse
-import retrofit2.Response
+import com.memo.utilitis.ResourceState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class NewsRepository  @Inject constructor(
     private val newsDataSource: NewsDataSource
 ){
-    suspend fun getTopHeadlines(country: String): Response<NewsResponse> {
-        return newsDataSource.getTopHeadlines(country)
+//    suspend fun getTopHeadlines(country: String): Response<NewsResponse> {
+//        return newsDataSource.getTopHeadlines(country)
+//    }
+    suspend fun getTopHeadlines(country: String): Flow<ResourceState<NewsResponse>> {
+        return flow {
+            emit(ResourceState.Loading())
+            val response = newsDataSource.getTopHeadlines(country)
+            if (response.isSuccessful && response.body() != null) {
+                emit(ResourceState.Success(response.body()!!))
+            } else {
+                emit(ResourceState.Error("An error occurred"))
+            }
+        }.catch { e ->
+            emit(ResourceState.Error(e.message ?: "An error occurred"))
+        }
     }
+
 }
--- utilitis/src/main/java/com/memo/utilitis/ResourceState.kt
+++ utilitis/src/main/java/com/memo/utilitis/ResourceState.kt
@@ -3,6 +3,6 @@ package com.memo.utilitis
 sealed class ResourceState<T> {
     class Loading<T> : ResourceState<T>()
     class Success<T>(val data: T) : ResourceState<T>()
-    class Error<T>(val message: String) : ResourceState<T>()
+    class Error<T>(val message: Any) : ResourceState<T>()
 
 }
