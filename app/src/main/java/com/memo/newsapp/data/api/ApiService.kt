0211bcb320583713f81a861048abe1a68e4d7706
Mohamed Hussein <mohussein1279@gmail.com>
2025-03-07 14:06:31 +0200

    ApiService has been created

---
+++ app/src/main/java/com/memo/newsapp/data/api/ApiService.kt
@@ -0,0 +1,15 @@
+package com.memo.newsapp.data.api
+
+import com.memo.newsapp.data.entity.NewsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("v2/top-headlines")
+    fun getTopHeadlines(
+        @Query("country") country: String,
+        @Query("apiKey") apiKey: String = "e9cd1aaa4d79449b84a86bad45cb4382"
+    ): Response<NewsResponse>
+}
+//GET https://newsapi.org/v2/top-headlines?country=us&apiKey=e9cd1aaa4d79449b84a86bad45cb4382
