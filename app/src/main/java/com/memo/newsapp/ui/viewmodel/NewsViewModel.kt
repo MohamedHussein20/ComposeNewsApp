bd08151c13fe094238ff7496b260287942bbfb52
Mohamed Hussein <mohussein1279@gmail.com>
2025-03-09 16:41:11 +0200

    MutableStateFlow and StateFlow

--- app/src/main/java/com/memo/newsapp/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/memo/newsapp/ui/viewmodel/NewsViewModel.kt
@@ -3,9 +3,13 @@ package com.memo.newsapp.ui.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.memo.newsapp.data.entity.NewsResponse
 import com.memo.newsapp.ui.repository.NewsRepository
+import com.memo.utilitis.ResourceState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,15 +18,20 @@ import javax.inject.Inject
 class NewsViewModel @Inject constructor(
     private val newsRepository: NewsRepository
 ) : ViewModel() {
+    private val _news: MutableStateFlow<ResourceState<NewsResponse>> =
+        MutableStateFlow(ResourceState.Loading())
+    val news: StateFlow<ResourceState<NewsResponse>> = _news
+
     fun getNews(country: String) {
         viewModelScope.launch(Dispatchers.IO) {
             newsRepository.getTopHeadlines(country)
-                .collectLatest {
+                .collectLatest { newsResponse ->
 
                 }
         }
     }
-    companion object{
+
+    companion object {
         const val TAG = "NewsViewModel"
     }
 }
