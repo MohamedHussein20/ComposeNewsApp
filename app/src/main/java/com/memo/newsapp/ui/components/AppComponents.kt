0e4df57690f7af86e1233ea2c2af2078b9071225
Mohamed Hussein <mohussein1279@gmail.com>
2025-03-10 15:55:07 +0200

    NewsList Added

--- app/src/main/java/com/memo/newsapp/ui/components/AppComponents.kt
+++ app/src/main/java/com/memo/newsapp/ui/components/AppComponents.kt
@@ -3,13 +3,23 @@ package com.memo.newsapp.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.memo.newsapp.data.entity.NewsResponse
 import com.memo.newsapp.ui.theme.Purple40
 
 @Composable
@@ -31,5 +41,25 @@ fun Loader(){
 
 }
 
-@Com
-fun new
+@Composable
+fun NewsList(response: NewsResponse) {
+    LazyColumn {
+        items(response.articles) { article ->
+            NormalTextComponent(textValue = article.title ?: "")
+        }
+    }
+}
+
+@Composable
+fun NormalTextComponent(textValue: String){
+    Text(modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp)
+        .wrapContentHeight()
+        ,text = textValue
+        ,style = TextStyle(
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Normal,
+        )
+    )
+}
